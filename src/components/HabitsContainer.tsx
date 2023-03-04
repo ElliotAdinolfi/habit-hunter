197f5dd3cef28f5dbd396f8567622df1d510e071
Elliot Adinolfi <elliot.adinolfi@gmail.com>
2023-03-03 19:49:39 -0500

    adds api call to get habit data

--- src/components/HabitsContainer.tsx
+++ src/components/HabitsContainer.tsx
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import styles from '@/styles/Habits.module.css';
 import NewHabit from '@/components/NewHabit';
+import axios from 'axios';
 
 const HabitsContainer = () => {
   const [habits, setHabits] = useState([]);
   const [newHabit, setNewHabit] = useState(false);
   const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      runGetHabits();
+    }
+  }, []);
+
+  function runGetHabits() {
+    (async function getHabits() {
+      const user_email = session?.user?.email;
+      const table = 'habits';
+
+      const habitData = await axios.get(
+        `/api/getHabit?user_email=${user_email}&table=${table}`
+      );
+      const habitList = habitData.data.map((habit: any) => {
+        return (
+          <div key={habit.id} className={styles.habitCard}>
+            <p>Habit: {habit.name}</p>
+            <p>Done Today: {String(habit.done_today)}</p>
+            <p>Streak: {habit.days_completed}</p>
+          </div>
+        );
+      });
+      setHabits(habitList);
+      console.log(habitList);
+    })();
+  }
 
   return (
     <div className={styles.habitContainer}>
