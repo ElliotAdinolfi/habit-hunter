4a22e0e4077c562571dae1cad0d9483ac54653b0
Elliot Adinolfi <elliot.adinolfi@gmail.com>
2023-03-06 10:15:27 -0500

    bug fix: fixed structure of daily reset cron job

--- src/pages/api/dailyReset.ts
+++ src/pages/api/dailyReset.ts
@@ -2,26 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PoolClient } from 'pg';
 import pool from '../../../lib/db';
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  async () => {
+  try {
     const client: PoolClient = await pool.connect();
-    try {
-      await client.query(`UPDATE habits SET
+    await client.query(`UPDATE habits SET
         done_today = false,
         streak = CASE WHEN done_today = false THEN 0 ELSE streak END;`);
-    } catch (error) {
-      if (process.env.NODE_ENV === 'production') {
-        console.log(error);
-        res.status(500).json({ message: 'Error in dailyReset API' });
-      } else {
-        res.status(500);
-      }
-    } finally {
-      client.release();
+    client.release();
+    res.status(200).json({ message: 'Daily Reset cron job started' });
+  } catch (error) {
+    if (process.env.NODE_ENV === 'production') {
+      console.log(error);
+      res.status(500).json({ message: 'Error in dailyReset API' });
+    } else {
+      res.status(500);
     }
-  };
-  res.status(200).json({ message: 'Daily Reset cron job started' });
+  }
 }
